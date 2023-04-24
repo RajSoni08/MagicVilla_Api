be6c261134a2056ea677c5bc7347c5b47e5dbcb8
Raj <raj.soni@kelltontech.com>
2023-04-24 10:46:58 +0530

    Section 1

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,19 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>() {
+                new Villa() {id =1 , Name= "Beach View"},
+            new Villa() {id =2 ,Name="Mpontain View"}};
+        }
+    }
+}
